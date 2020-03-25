b6658465c72e1600093e594d8a816bcf6e49bb10
Brett Taylor <brett_21@hotmail.com>
2020-03-25 10:17:31 -0400

    Add files via upload

---
+++ Analysis.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace DTEDAnalysis
+{
+    class Analysis
+    {
+        // Compare
+        public void Compare(double startLat, double startLon, double endLat, double endLon, string SRTM_fold, string DTED_fold)
+        {
+            string extension = ".dt2";
+
+            if (startLat > endLat)
+                Debug.WriteLine("startLat needs to be less than endLat");
+
+            if (startLon > endLon)
+                Debug.WriteLine("startLon needs to be less than endLon");
+
+            int roundStartLat = (int)Math.Floor(startLat);
+            int roundEndLat = (int)Math.Floor(endLat);
+
+            int roundStartLon = (int)Math.Floor(startLon);
+            int roundEndLon = (int)Math.Floor(endLon);
+
+
+                for (int curLon = roundStartLon; curLon <= roundEndLon; curLon++)
+            {
+                //TODO: May need to add leading zeros
+                string curLonStr;
+                if (curLon < 0)
+                    curLonStr = "W" + (-1 * curLon).ToString("D3");
+                else
+                    curLonStr = "E" + curLon.ToString("D3");
+
+                for (int curLat = roundStartLat; curLat <= roundEndLat; curLat++)
+                {
+                    //TODO: May need to add leading zeros
+                    string curLatStr;
+                    if (curLat < 0)
+                        curLatStr = "S" + (-1 * curLat).ToString("D2");
+                    else
+                        curLatStr = "N" + curLat.ToString("D2");
+
+                    string SRTM_path = SRTM_fold + curLonStr + "\\" + curLatStr + extension;
+                    string DTED_path = DTED_fold + curLonStr + "\\" + curLatStr + extension;
+
+                    Debug.WriteLine("Analysis::Compare(): SRTM Path: " + SRTM_path);
+                    Debug.WriteLine("Analysis::Compare(): DTED Path: " + DTED_path);
+
+                    cDTED SRTM = new cDTED();
+                    cDTED DTED = new cDTED();
+
+                    Debug.WriteLine("Analysis::Compare(): Loading SRTM File...");
+                    SRTM.readFile(SRTM_path);
+
+                    Debug.WriteLine("Analysis::Compare(): Loading DTED File...");
+                    DTED.readFile(DTED_path);
+
+                    Debug.WriteLine("SRTM Size: " + SRTM.posts.Count + " DTED Size: " + DTED.posts.Count);
+
+                    AnalyzeArea(SRTM, DTED, startLat, endLon, 250);
+                    //Analyze(SRTM, DTED);
+                }
+            }
+
+        }
+
+        // Analyze
+        public void Analyze(cDTED SRTM, cDTED DTED)
+        {
+            if (SRTM.posts.Count != DTED.posts.Count)
+            {
+                Debug.WriteLine("Analysis:Analyze() - SRTM and DTED sizes do not agree");
+                return;
+            }
+
+            var csv = new System.Text.StringBuilder();
+
+            Post largest = new Post(-10000, 0, 0);
+
+            double average_delta = 0;
+
+
+            //for (int m = 0; m < SRTM.posts.Count; m++)
+            for (int m = 400000; m < 440000; m++)
+            {
+
+                int difference = SRTM.posts[m]._alt_ft - DTED.posts[m]._alt_ft;
+                //Debug.WriteLine("SRTM: " + SRTM.posts[m]._alt_ft + "- DTED: " + DTED.posts[m]._alt_ft + " = " + difference);
+
+                if (m % 1000 == 0)
+                    Debug.WriteLine("Writing: " + m);
+
+                var newLine = string.Format("{0},{1},{2},{3},{4},{5},{6}", SRTM.posts[m]._lat, SRTM.posts[m]._lon, SRTM.posts[m]._alt_ft, DTED.posts[m]._lat, DTED.posts[m]._lon, DTED.posts[m]._alt_ft, difference);
+                csv.AppendLine(newLine);
+
+                File.WriteAllText("CompareTest.csv", csv.ToString());
+            }
+        }
+
+
+        public void AnalyzeArea(cDTED SRTM, cDTED DTED, double startLat, double endLon, int size)
+        {
+            int[,] DTED_points = new int[size, size];
+            int[,] SRTM_points = new int[size, size];
+
+            double[] longitudes = new double[size];
+            double[] latitudes = new double[size];
+
+
+            int lonIndex = -1;
+            //Find first point
+            for (int i = 0; i <= SRTM.posts.Count; i++) //Loop through Lon
+            {
+                if (SRTM.posts[i]._lon >= endLon) //File is index to west, so find furthest west of the two (start/end)
+                {
+                    lonIndex = i;
+                    break;
+                }
+            }
+            if (lonIndex == -1)
+            {
+                Debug.WriteLine("Analysis:AnalyzeArea() - Start Longitude not found.");
+            }
+
+            int latIndex = -1;
+            //Find first point
+            for (int i = 0; i <= SRTM.posts.Count; i++) //Loop through Lon
+            {
+                if (SRTM.posts[i]._lat >= startLat)
+                {
+                    latIndex = i;
+                    break;
+                }
+            }
+            if (latIndex == -1)
+            {
+                Debug.WriteLine("Analysis:AnalyzeArea() - Start Latitude not found.");
+            }
+
+            Debug.WriteLine("LatIdx: " + latIndex + " LonIdx: " + lonIndex);
+
+
+            //Actually pull at points
+            for (int i = size - 1; i >= 0; i--) //Loop through Lon
+            {
+                int row_idx = lonIndex + i * DTED.lon_cnt;
+
+                //for (int j = size - 1; j >= 0; j--) //Loop through lat
+                for (int j = 0; j < size; j++) //Loop through lat
+                {
+                    int idx = row_idx + latIndex + j;
+
+                    //Debug.WriteLine("SRTM: (" + SRTM.posts[idx]._lat + "," + SRTM.posts[idx]._lon + "): " + SRTM.posts[idx]._alt_ft );
+                    SRTM_points[j, i] = SRTM.posts[idx]._alt_ft;
+                    DTED_points[j, i] = DTED.posts[idx]._alt_ft;
+                }
+            }
+
+            //Doing this seperate to try to save some processing time
+            for (int i = size - 1; i >= 0; i--) //Loop through Lon
+            {
+                int idx = lonIndex + i * DTED.lon_cnt;
+                longitudes[i] = SRTM.posts[idx]._lon;
+            }
+            //for (int j = size - 1; j >= 0; j--) //Loop through lat
+            for (int j = 0; j < size; j++) //Loop through lat
+            {
+                int idx = latIndex + j;
+                latitudes[j] = SRTM.posts[idx]._lat;
+            }
+
+            /*
+            for(int i = 0;i < size;i++)
+            {
+                Debug.WriteLine("Latitudes: " + i + " " + latitudes[i]);
+                Debug.WriteLine("Longitudes: " + i + " " + longitudes[i]);
+            }
+            */
+
+
+            var SRTM_CSV = new System.Text.StringBuilder();
+            var DTED_CSV = new System.Text.StringBuilder();
+            var Diff_CSV = new System.Text.StringBuilder();
+
+            string line = "";
+            for (int i = 0; i < size; i++)
+            {
+                line = line + "," + longitudes[i];
+
+            }
+            SRTM_CSV.AppendLine(line);
+            DTED_CSV.AppendLine(line);
+            Diff_CSV.AppendLine(line);
+
+            string dted_line = "";
+            string srtm_line = "";
+            string diff_line = "";
+
+            //lat, lon is order
+            for (int i = 0; i < size; i++)
+            {
+                srtm_line = latitudes[i] + ",";
+                dted_line = latitudes[i] + ",";
+                diff_line = latitudes[i] + ",";
+
+                for (int j = 0; j < size; j++)
+                {
+                    srtm_line = srtm_line + SRTM_points[i, j] + ",";
+                    dted_line = dted_line + DTED_points[i, j] + ",";
+                    diff_line = diff_line + (SRTM_points[i, j] - DTED_points[i, j]) + ",";
+
+                    //Debug.WriteLine(dted_line);
+                }
+
+                SRTM_CSV.AppendLine(srtm_line);
+                DTED_CSV.AppendLine(dted_line);
+                Diff_CSV.AppendLine(diff_line);
+            }
+            File.WriteAllText("SRTM_Area.csv", SRTM_CSV.ToString());
+            File.WriteAllText("DTED_Area.csv", DTED_CSV.ToString());
+            File.WriteAllText("Diff_Area.csv", Diff_CSV.ToString());
+        }
+    }
+}
+
+    /*
+    // Analyze
+    public void AnalyzeArea(cDTED SRTM, cDTED DTED, int middlePoint, int size)
+    { 
+        {
+            int offset = (int)Math.Round((double)size * 0.5);
+
+            int lag = middlePoint - offset;
+
+            if((size*DTED.lon_cnt + lag > SRTM.posts.Count))
+            {
+                Debug.WriteLine("Analysis:Analyze() - Size/MiddlePoint do not fit.  Max size is " + size*size);
+                return;
+            }
+
+            if(lag < 0)
+            {
+                Debug.WriteLine("Analysis:Analyze() - Size/MiddlePoint result in less than zero.");
+                return;
+            }
+
+            int[,] DTED_points = new int[size, size];
+            int[,] SRTM_points = new int[size, size];
+
+            double[] longitudes = new double[size];
+            double[] latitudes = new double[size];
+
+
+            //Actually pull at points
+            for (int i = size - 1;i >= 0;i--) //Loop through Lon
+            {
+                int row_idx = lag + i * DTED.lon_cnt;
+
+                for (int j = size - 1; j >= 0; j--) //Loop through lat
+                {
+                    int idx = row_idx + lag + j;
+
+                    //Debug.WriteLine("SRTM: (" + SRTM.posts[idx]._lat + "," + SRTM.posts[idx]._lon + "): " + SRTM.posts[idx]._alt_ft );
+                    SRTM_points[j, i] = SRTM.posts[idx]._alt_ft;
+                    DTED_points[j ,i] = DTED.posts[idx]._alt_ft;
+                }
+            }
+
+            //Doing this seperate to try to save some processing time
+            for (int i = size-1; i >= 0; i--) //Loop through Lon
+            {
+                int idx = lag + i * DTED.lon_cnt;
+                longitudes[i] = SRTM.posts[idx]._lon;
+            }
+            for (int j = size - 1; j >= 0; j--) //Loop through lat
+            {
+                int idx = lag + j;
+                latitudes[j] = SRTM.posts[idx]._lat;
+            }
+
+            /*
+            for(int i = 0;i < size;i++)
+            {
+                Debug.WriteLine("Latitudes: " + i + " " + latitudes[i]);
+                Debug.WriteLine("Longitudes: " + i + " " + longitudes[i]);
+            }
+            
+
+
+            var SRTM_CSV = new System.Text.StringBuilder();
+            var DTED_CSV = new System.Text.StringBuilder();
+            var Diff_CSV = new System.Text.StringBuilder();
+
+            string line = "";
+            for (int i = 0; i < size; i++)
+            {
+                line = line + "," + longitudes[i];
+
+            }
+            SRTM_CSV.AppendLine(line);
+            DTED_CSV.AppendLine(line);
+            Diff_CSV.AppendLine(line);
+
+            string dted_line = "";
+            string srtm_line = "";
+            string diff_line = "";
+
+            //lat, lon is order
+            for (int i = 0;i < size;i++)
+            {
+                srtm_line = latitudes[i] + ",";
+                dted_line = latitudes[i] + ",";
+                diff_line = latitudes[i] + ",";
+
+                for (int j = 0;j < size;j++)
+                {
+                    srtm_line = srtm_line + SRTM_points[i, j] + ",";
+                    dted_line = dted_line + DTED_points[i, j] + ",";
+                diff_line = diff_line + (SRTM_points[i, j] - DTED_points[i, j]) + ",";
+
+                    //Debug.WriteLine(dted_line);
+                }
+
+                SRTM_CSV.AppendLine(srtm_line);
+                DTED_CSV.AppendLine(dted_line);
+                Diff_CSV.AppendLine(diff_line);
+            }
+            File.WriteAllText("SRTM_Area.csv", SRTM_CSV.ToString());
+            File.WriteAllText("DTED_Area.csv", DTED_CSV.ToString());
+            File.WriteAllText("Diff_Area.csv", DTED_CSV.ToString());
+        }
+
+    }
+}
+*/
---
+++ DTED.cs
@@ -0,0 +1,262 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Diagnostics;
+using System;
+
+
+namespace DTEDAnalysis
+{
+    
+    class Post
+    {
+        public int _alt_m;
+        public int _alt_ft;
+        public double _lat;
+        public double _lon;
+
+        //Constructor
+        public Post(int alt_m, double lat, double lon)
+        {
+            _alt_m = alt_m;
+            _alt_ft = (int)(alt_m * 3.28084);
+            _lat = lat;
+            _lon = lon;
+        }
+    }
+
+    class cDTED
+    {
+        public List<Post> posts;
+        string filename;  //Includes path
+
+        public int lon_cnt;
+        public int lat_cnt;
+
+        public double lat_orig;
+        public double lon_orig;
+
+        public double lat_intrvl;
+        public double lon_intrvl;
+
+        //Constructor
+        public cDTED()
+        {
+            posts = new List<Post>();
+        }
+        
+        //Data Loader
+        public unsafe void readFile(string fileName)
+        {
+            using (BinaryReader reader = new BinaryReader(File.Open(fileName, FileMode.Open)))
+            {
+                Debug.WriteLine("DTED::readFile(): File open...");
+
+                int size = 80 + 648 + 2700; //Size of intro stuff
+                char[] hdr = new char[size];
+                hdr = reader.ReadChars(size);
+                
+                //User Header Label
+                char[] sub = new char[3];
+                Array.Copy(hdr, sub, 3);
+                string UHL = new string(sub);
+
+                //Number of lines of longitude - Skip to 48
+                sub = new char[4];
+                Array.Copy(hdr, 47, sub, 0, 4);
+                lon_cnt = Int32.Parse(new string(sub));
+
+                //Number of lines of latitude
+                sub = new char[4];
+                Array.Copy(hdr, 51, sub, 0, 4);
+                lat_cnt = Int32.Parse(new string(sub));
+
+                Debug.WriteLine("DTED::readFile(): Num lon: " + lon_cnt + " Num lat: " + lat_cnt);
+
+                //Latitude Origin - per standard this is SW corner (lower left) of the data
+                sub = new char[8];
+                Array.Copy(hdr, 12, sub, 0, 8);
+                string lat_orig_str = new string(sub);
+
+                lat_orig = Double.Parse(lat_orig_str.Substring(0, 7)) / 10000; //Not strictly true, assumign always even degrees
+                string lat_hemi = lat_orig_str.Substring(7, 1);
+                if (lat_hemi == "S")
+                    lat_orig = lat_orig * -1;
+
+
+                sub = new char[8];
+                Array.Copy(hdr, 4, sub, 0, 8);
+                string lon_orig_str = new string(sub);
+
+                lon_orig = Double.Parse(lon_orig_str.Substring(0, 7)) / 10000;
+                string lon_hemi = lon_orig_str.Substring(7, 1);
+                if (lon_hemi == "W")
+                    lon_orig = lon_orig * -1;
+
+                Debug.WriteLine("DTED::readFile(): Origin: (" + lat_orig + " , " + lon_orig + ")");
+
+
+                sub = new char[3];
+                Array.Copy(hdr, 80, sub, 0, 3);
+                string DSI = new string(sub);
+                Debug.WriteLine("DSI: " + DSI);
+
+                sub = new char[4];
+                Array.Copy(hdr, 80 + 273, sub, 0, 4);
+                lon_intrvl = Double.Parse(new string(sub)) * 2.77778 * Math.Pow(10.0, -5.0);
+
+                sub = new char[4];
+                Array.Copy(hdr, 80 + 277, sub, 0, 4);
+                lat_intrvl = Double.Parse(new string(sub)) * 2.77778 * Math.Pow(10.0, -5.0);
+
+                Debug.WriteLine("DTED::readFile(): Lat intrvl: " + lat_intrvl + " Lon intrvl: " + lon_intrvl);
+
+
+                sub = new char[3];
+                Array.Copy(hdr, 80+648, sub, 0, 3);
+                string ACC = new string(sub);
+                Debug.WriteLine("DTED::readFile(): ACC: " + ACC);
+
+                //Loop through each longitude band
+                for (int i = 0; i < lon_cnt; i++)
+                {
+                    size = 12 + 2 * lat_cnt;
+                    byte[] data = new byte[size];
+                    data= reader.ReadBytes(size);
+
+                    byte[] sub1 = new byte[4];
+                    Array.Copy(data, 0, sub1, 0, 4);
+                    sub1[0] = 0;
+                    if (BitConverter.IsLittleEndian)
+                        Array.Reverse(sub1);
+                    int blk_cnt = BitConverter.ToInt32(sub1, 0);
+
+                    /*
+                    sub1 = new byte[2];
+                    Array.Copy(data, 4, sub1, 0, 2);
+                    if (BitConverter.IsLittleEndian)
+                        Array.Reverse(sub1);
+                    int temp_lon_cnt = BitConverter.ToInt16(sub1, 0);
+
+                    sub1 = new byte[2];
+                    Array.Copy(data, 6, sub1, 0, 2);
+                    if (BitConverter.IsLittleEndian)
+                        Array.Reverse(sub1);
+                    int temp_lat_cnt = BitConverter.ToInt16(sub1, 0);
+
+                    Debug.WriteLine("Lon Cnt: " + temp_lon_cnt + " lat cnt: " + temp_lat_cnt);
+                    */
+
+                    sub1 = new byte[2];
+                    Array.Copy(data, 4, sub1, 0, 2);
+                    if (BitConverter.IsLittleEndian)
+                        Array.Reverse(sub1);
+                    short curLon = BitConverter.ToInt16(sub1, 0);
+
+                    sub1 = new byte[2];
+                    Array.Copy(data, 6, sub1, 0, 2);
+                    if (BitConverter.IsLittleEndian)
+                        Array.Reverse(sub1);
+                    short curLat = BitConverter.ToInt16(sub1, 0);
+
+                    //Debug.WriteLine("Lon Cnt, Lat Cnt: " + curLon + " " + curLat);
+
+
+                    if (blk_cnt % 250 == 0)
+                        Debug.WriteLine("DTED::readFile(): Blk Cnt: " + blk_cnt);
+
+
+
+                    //Loop through each latitude n the longitude band
+                    for (int j = 0;j < lat_cnt;j++)
+                    {
+                        sub1 = new byte[2];
+                        Array.Copy(data, 8+2*j, sub1, 0, 2);
+                        if (BitConverter.IsLittleEndian)
+                            Array.Reverse(sub1);
+                        short alt_m = BitConverter.ToInt16(sub1, 0);
+                        double lat = j * lat_intrvl + lat_orig;
+                        double lon = i * lon_intrvl + lon_orig;
+
+                        //Debug.WriteLine("(" + lat + " , " + lon + ") = " + alt_ft);
+                        posts.Add(new Post(alt_m, lat, lon));
+                    }
+                }
+            }
+        }
+
+        public void toCSV(string csvName)
+        {
+            var csv = new System.Text.StringBuilder();
+
+            foreach(Post x in posts)
+            {
+                var newLine = string.Format("{0},{1},{2}", x._lat, x._lon, x._alt_m);
+                csv.AppendLine(newLine);
+            }
+            File.WriteAllText(csvName, csv.ToString());
+        }
+
+        public int getClosest(double lat, double lon)
+        {
+            double adj_lat = Math.Abs(lat - lat_orig);
+            double adj_lon = Math.Abs(lon - lon_orig);
+
+            int lon_index = (int)(adj_lon / lon_intrvl);
+            int lat_index = (int)(adj_lat / lat_intrvl);
+
+            int index = lon_index * lon_cnt + lat_index;
+            Debug.WriteLine("Index is: " + index);
+
+            for (int i = index - 4; i < index+4;i++)
+                Debug.WriteLine("(" + posts[i]._lat + " , " + posts[i]._lon + ") = " + posts[i]._alt_ft);
+
+            return posts[index]._alt_ft;
+        }
+    }
+
+}
+
+
+/*Mostly debug code
+sub = new char[7];
+Array.Copy(hdr, 80+205, sub, 0, 7);
+string lat_str = new string(sub);
+
+sub = new char[8];
+Array.Copy(hdr, 80+212, sub, 0, 8);
+string lon_str = new string(sub);
+
+Debug.WriteLine("SW: (" + lat_str + " , " + lon_str + ")");
+
+sub = new char[7];
+Array.Copy(hdr, 80 + 220, sub, 0, 7);
+lat_str = new string(sub);
+
+sub = new char[8];
+Array.Copy(hdr, 80 + 227, sub, 0, 8);
+lon_str = new string(sub);
+
+Debug.WriteLine("NW: (" + lat_str + " , " + lon_str + ")");
+
+sub = new char[7];
+Array.Copy(hdr, 80 + 235, sub, 0, 7);
+lat_str = new string(sub);
+
+sub = new char[8];
+Array.Copy(hdr, 80 + 242, sub, 0, 8);
+lon_str = new string(sub);
+
+Debug.WriteLine("NE: (" + lat_str + " , " + lon_str + ")");
+
+
+sub = new char[7];
+Array.Copy(hdr, 80 + 250, sub, 0, 7);
+lat_str = new string(sub);
+
+sub = new char[8];
+Array.Copy(hdr, 80 + 257, sub, 0, 8);
+lon_str = new string(sub);
+
+Debug.WriteLine("SE: (" + lat_str + " , " + lon_str + ")");
+end mostly debug*/
